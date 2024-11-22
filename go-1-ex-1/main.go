20657303f7723c4f6ba4a19c4182335a49165cf9
VORNAME NACHNAME <VORNAME_NACHNAME@sluz.ch>
2024-11-22 15:55:41 +0100

    exercise 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var firstName, lastName string = "Yusuf", "Er"
+var dayOfBirth, monthOfBirth, yearOfBirth, numberOfSiblings int = 11, 01, 2007, 2
+var heightInMeters float32 = 1.80
+var zodiacSign rune = '\u2651'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
